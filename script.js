64cb8fce4b5ef71064c6c1fee807122320ae48cd
Ayen <87607693+Ayen11@users.noreply.github.com>
2023-07-05 15:04:35 +0300

    added a completed function when pressing on a task
    also made the input bar clear itself after creating a task

--- script.js
+++ script.js
@@ -12,12 +12,23 @@ document.querySelector("#push").onclick = function () {
         </button> 
         </div>
     `;
+    document.querySelector("#newtask input").value = "";
+    /* clears the tasks input*/
 
     var current_tasks = document.querySelectorAll(".delete");
-
     for (var i = 0; i < current_tasks.length; i++) {
       current_tasks[i].onclick = function () {
         this.parentNode.remove();
+      };
+    }
+
+    var tasks = document.querySelectorAll(".task");
+
+    for (var i = 0; i < tasks.length; i++) {
+      tasks[i].onclick = function () {
+        this.classList.toggle(
+          "completed"
+        ); /* since this applies a line-through to all texts it also does it to the del button. will be fixed once it's switched into an icon*/
       };
     }
   }
