3e5d6acdf3a021fa0f1d03e8948f6eb2f7f374cb
Ayen <87607693+Ayen11@users.noreply.github.com>
2023-07-08 17:09:59 +0300

    added basic local storage array

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+/*display date */
+function displayDate() {
+  let date = new Date();
+  date = date.toString().split(" ");
+  document.querySelector("#date").innerHTML =
+    date[1] + " " + date[2] + " " + date[3];
+}
+
+window.onload = function () {
+  displayDate();
+  console.log(itemsArray);
+};
+
+const itemsArray = localStorage.getItem("items")
+  ? JSON.parse(localStorage.getItem("items"))
+  : [];
+
 document.querySelector("#push").onclick = function () {
   if (document.querySelector("#newtask input").value.length == 0) {
     alert("Please Enter a Task");
   } else {
+    /*add the task html to the page */
     document.querySelector("#tasks").innerHTML += `
         <div class="task"
         <span id="taskname">
@@ -12,9 +30,13 @@ document.querySelector("#push").onclick = function () {
         </button> 
         </div>
     `;
-    document.querySelector("#newtask input").value = "";
-    /* clears the tasks input*/
+    /*add the item to the local storage array */
+    createItem(document.querySelector("#newtask input"));
 
+    /* clears the tasks input*/
+    document.querySelector("#newtask input").value = "";
+
+    /*deletes task */
     var current_tasks = document.querySelectorAll(".delete");
     for (var i = 0; i < current_tasks.length; i++) {
       current_tasks[i].onclick = function () {
@@ -24,6 +46,7 @@ document.querySelector("#push").onclick = function () {
 
     var tasks = document.querySelectorAll(".task");
 
+    /*marks as done */
     for (var i = 0; i < tasks.length; i++) {
       tasks[i].onclick = function () {
         this.classList.toggle(
@@ -33,3 +56,8 @@ document.querySelector("#push").onclick = function () {
     }
   }
 };
+
+function createItem(item) {
+  itemsArray.push(item.value);
+  localStorage.setItem("items", JSON.stringify(itemsArray));
+}
